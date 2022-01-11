3d86cd48626931ddda86ff02f9fac6cb94bdf394
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-11 15:45:07 +0100

    Added the MainApplication class and Tests

---
+++ src/main/java/dev/gxsoft/blogrestapi2/BlogRestApi2Application.java
@@ -0,0 +1,13 @@
+package dev.gxsoft.blogrestapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogRestApi2Application.class, args);
+    }
+
+}
